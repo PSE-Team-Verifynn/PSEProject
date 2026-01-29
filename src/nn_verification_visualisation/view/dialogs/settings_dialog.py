9769e9acfcaacdfef857565c2314c77c7579b52d
Eli-dd <90352951+Eli-dd@users.noreply.github.com>
2026-01-29 20:15:39 +0100

    Started first settings

--- src/nn_verification_visualisation/view/dialogs/settings_dialog.py
+++ src/nn_verification_visualisation/view/dialogs/settings_dialog.py
@@ -1,6 +1,6 @@
 from typing import List, Callable
 
-from PySide6.QtWidgets import QWidget, QLabel
+from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout, QScrollArea, QHBoxLayout
 
 from nn_verification_visualisation.view.dialogs.dialog_base import DialogBase
 from nn_verification_visualisation.view.dialogs.settings_option import SettingsOption
@@ -9,8 +9,23 @@ class SettingsDialog(DialogBase):
     settings: List[SettingsOption]
 
     def __init__(self, on_close: Callable[[], None]):
+        self.settings = [
+            SettingsOption("Test", QLabel("change"), lambda _: print("test")),
+            SettingsOption("Test2", QWidget(), lambda _: print("test")),
+        ]
+
         super().__init__(on_close, "Settings")
-        print("SettingsDialog init")
 
     def get_content(self) -> QWidget:
-        return QLabel("Settings")
+        widget = QScrollArea()
+        layout = QVBoxLayout()
+
+        for setting in self.settings:
+            setting_layout = QHBoxLayout()
+            setting_layout.addWidget(QLabel(setting.name))
+            setting_layout.addStretch()
+            setting_layout.addWidget(setting.settings_changer)
+            layout.addLayout(setting_layout)
+
+        widget.setLayout(layout)
+        return widget
--- src/nn_verification_visualisation/view/dialogs/settings_option.py
+++ src/nn_verification_visualisation/view/dialogs/settings_option.py
@@ -7,3 +7,8 @@ class SettingsOption:
     name: str
     settings_changer: QWidget
     on_change: Callable[[str], None]
+
+    def __init__(self, name: str, settings_changer: QWidget, on_change: Callable[[str], None]):
+        self.name = name
+        self.settings_changer = settings_changer
+        self.on_change = on_change
